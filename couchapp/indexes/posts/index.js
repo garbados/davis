88ca607ccf746dbcb83f341864d55125541abc2a
Max Thayer <garbados@gmail.com>
2014-02-27 15:39:57 -0500

    fixed search index

--- couchapp/indexes/posts/index.js
+++ couchapp/indexes/posts/index.js
@@ -4,19 +4,20 @@ function (doc) {
   }
 
   if (doc.type) {
+    // index every doc field by name
     Object
     .keys(doc)
     .filter(function (key) {
-      return (key[0] === '_');
+      return (key[0] !== '_');
     })
     .forEach(function (key) {
-      if (doc[key].split) {
+      if (!doc[key].forEach) {
         // if it's a string, index it without modification
-        index(key, doc[key], { facet: true });
+        index(key, doc[key]);
       } else {
         // if it's an array, index each element
         doc[key].forEach(function (value) {
-          index(key, value, { facet: true });
+          index(key, value);
         })
       }
     });
